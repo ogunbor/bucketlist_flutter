bd82db7e19020d89d8f4ffa13e417a75ff45a153
mompei <ogunborjerry@gmail.com>
2024-04-12 00:59:23 +0100

    Add page route for adding bucketlist

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bucketlist/addBucketList.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
@@ -47,6 +48,15 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return AddBucketListScreen();
+          }));
+        },
+        shape: CircleBorder(),
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: const Text('Bucket List'),
         actions: [
